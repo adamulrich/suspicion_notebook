ec72b06ce4a5502dbf646498d03514e15ae7a400
Adam Ulrich <adamulrich@hotmail.com>
2024-08-24 15:04:54 -0700

    resize on pinchzoom

--- suspicion.js
+++ suspicion.js
@@ -1,3 +1,15 @@
 window.onbeforeunload = function() {
     return "Data will be lost if you leave the page, are you sure?";
   };
+
+
+visualViewport.onresize = () => {
+  var scale = visualViewport.scale;
+  const cardClasses = document.querySelectorAll('input[type=text]');
+  var new_width = 100 * scale *1;
+  new_width = new_width.toString() + "px"
+  cardClasses.forEach(element => {
+    element.style.width =  new_width;
+    
+  });
+};
