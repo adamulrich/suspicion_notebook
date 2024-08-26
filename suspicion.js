f7deb5d999719031ce3c66e18756304cef7cf926
Adam Ulrich <adamulrich@hotmail.com>
2024-08-26 13:14:10 -0700

    adjust styling and colors

--- suspicion.js
+++ suspicion.js
@@ -2,7 +2,7 @@ window.onbeforeunload = function() {
     return "Data will be lost if you leave the page, are you sure?";
   };
 
-var playerCount = 1;
+var playerCount = 0;
 
 visualViewport.onresize = () => {
   var scale = visualViewport.scale;
@@ -53,58 +53,69 @@ function addPlayer() {
   </div>
 
   <label class="label">Player <#number#> Color</label>
-  <select name="player1_color" id="player<#number#>_color">
+  <select class="player_color_selection" name="player1_color" id="player<#number#>_color" onchange="setColor()" style="background-color:#00f2ff">
       <option default value="unknown" >unknown</option>
-      <option value="blue" style="background-color: #0053a5;color: aliceblue;">blue</option>
-      <option value="brown" style="background-color: #bd5700;color: aliceblue;">brown</option>
-      <option value="gray" style="background-color: #bdbab4;color: aliceblue">gray</option>
-      <option value="green" style="background-color: #b1b801;color: aliceblue;" >green</option>
-      <option value="orange" style="background-color: #e99303;color: aliceblue;">orange</option>
       <option value="pink" style="background-color: #f697c8;color: aliceblue;">pink</option>
-      <option value="purple" style="background-color: #5b157c;color: aliceblue;">purple</option>
       <option value="red" style="background-color: #b90202;color: aliceblue;">red</option>
-      <option value="white" style="background-color: white;color: black;">white</option>
+      <option value="brown" style="background-color: #bd5700;color: aliceblue;">brown</option>
+      <option value="orange" style="background-color: #e99303;color: aliceblue;">orange</option>
       <option value="yellow" style="background-color: #fff703;color: black;">yellow</option>
+      <option value="green" style="background-color: #b1b801;color: aliceblue;" >green</option>
+      <option value="blue" style="background-color: #0053a5;color: aliceblue;">blue</option>
+      <option value="purple" style="background-color: #5b157c;color: aliceblue;">purple</option>
+      <option value="gray" style="background-color: #bdbab4;color: aliceblue">gray</option>
+      <option value="white" style="background-color: white;color: black;">white</option>
+      
   </select>
-  <div class="card-checkbox">
-      <input type="checkbox" name="blue" id="blue<#number#>">
-      <label class= "checkbox-label" style="background-color: #0053a5;color: aliceblue;" for="blue<#number#>">blue</label>
-  </div>
-  <div class="card-checkbox">
-      <input type="checkbox" name="brown" id="brown<#number#>">
-      <label class= "checkbox-label" style="background-color: #bd5700;color: aliceblue;" for="brown<#number#>">brown</label>
-  </div>
-  <div class="card-checkbox">
-      <input type="checkbox" class="checkbox" name="gray" id="gray<#number#>" >
-      <label class="checkbox-label" style="background-color: #bdbab4;color: aliceblue" for="gray<#number#>">gray</label>
-  </div>
-  <div class="card-checkbox">
-      <input type="checkbox" name="green" id="green<#number#>">
-      <label class= "checkbox-label" style="background-color: #b1b801;color: aliceblue;" for="green<#number#>">green</label>
-  </div>
-  <div class="card-checkbox">
-      <input type="checkbox" name="orange" id="orange<#number#>">
-      <label class= "checkbox-label" style="background-color: #e99303;color: aliceblue;" for="orange<#number#>">orange</label>
-  </div>
+  <!-- pink -->
   <div class="card-checkbox">
       <input type="checkbox" name="pink" id="pink<#number#>">
       <label class= "checkbox-label" style="background-color: #f697c8;color: aliceblue;" for="pink<#number#>">pink</label>
   </div>
-  <div class="card-checkbox">
-      <input type="checkbox" name="purple" id="purple<#number#>">
-      <label  class= "checkbox-label" style="background-color: #5b157c;color: aliceblue;" for="purple<#number#>">purple</label>
-  </div>
+  <!-- red -->
   <div class="card-checkbox">
       <input type="checkbox" name="red" id="red<#number#>">
       <label class= "checkbox-label" style="background-color: #b90202;color: aliceblue;" for="red<#number#>">red</label>
   </div>
+  <!-- brown -->
   <div class="card-checkbox">
-      <input type="checkbox" name="white" id="white<#number#>">
-      <label class= "checkbox-label" style="background-color: white;color: black;" for="white<#number#>">white</label>
+      <input type="checkbox" name="brown" id="brown<#number#>">
+      <label class= "checkbox-label" style="background-color: #bd5700;color: aliceblue;" for="brown<#number#>">brown</label>
   </div>
+  <!-- orange -->
+  <div class="card-checkbox">
+      <input type="checkbox" name="orange" id="orange<#number#>">
+      <label class= "checkbox-label" style="background-color: #e99303;color: aliceblue;" for="orange<#number#>">orange</label>
+  </div>
+  <!-- yellow -->
   <div class="card-checkbox">
       <input type="checkbox" name="yellow" id="yellow<#number#>">
       <label class= "checkbox-label" style="background-color: #fff703;color: black;" for="yellow<#number#>">yellow</label>
+  </div>
+  <!-- green -->
+  <div class="card-checkbox">
+      <input type="checkbox" name="green" id="green<#number#>">
+      <label class= "checkbox-label" style="background-color: #b1b801;color: aliceblue;" for="green<#number#>">green</label>
+  </div>
+  <!-- blue -->
+  <div class="card-checkbox">
+      <input type="checkbox" name="blue" id="blue<#number#>">
+      <label class= "checkbox-label" style="background-color: #0053a5;color: aliceblue;" for="blue<#number#>">blue</label>
+  </div>
+  <!-- puple -->
+  <div class="card-checkbox">
+      <input type="checkbox" name="purple" id="purple<#number#>">
+      <label  class= "checkbox-label" style="background-color: #5b157c;color: aliceblue;" for="purple<#number#>">purple</label>
+  </div>
+  <!-- gray -->
+  <div class="card-checkbox">
+      <input type="checkbox" class="checkbox" name="gray" id="gray<#number#>" >
+      <label class="checkbox-label" style="background-color: #bdbab4;color: aliceblue" for="gray<#number#>">gray</label>
+  </div>
+  <!-- white -->
+  <div class="card-checkbox">
+      <input type="checkbox" name="white" id="white<#number#>">
+      <label class= "checkbox-label" style="background-color: white;color: black;" for="white<#number#>">white</label>
   </div>
   
 </div>`
@@ -124,5 +135,16 @@ function eliminateColor(name) {
   selection.forEach( (item) => {
     item.checked = document.getElementById(name).checked;
   })
+}
 
+function setColor() {
+  const player_colors = document.getElementsByClassName("player_color_selection");
+  console.log(player_colors.length);
+  [...player_colors].forEach( (item) => {
+    if (item.value != "unknown") {
+      item.style="background-color:"+item.value;
+    }
+    else
+    {  item.style="background-color:#00f2ff";}
+  })
 }
